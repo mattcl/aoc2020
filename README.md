f8dd7f9bab10a0011f1b75c74ef6f4892fe76e63
Matt Chun-Lum <matt@questionable.engineering>
2020-12-01 19:51:24 -0800

    Problem 1 and run script

---
+++ README.md
@@ -0,0 +1,18 @@
+AOC 2020
+========
+
+Project structure is a little weird. In order to keep everything runnable,
+solution execution is managed by examples, with the following directory
+structure:
+```
+examples/
+   001_<name>_/ # -> day 1
+   002_<name>_/ # -> day 2
+   ...
+```
+
+Supporting library code lives in `src/` as would be standard for a rust library.
+This allows for testing implementations with `cargo test` and such while still
+providing a convenient way to execute specific solutions via the supplied `run`
+script with `./run 001` or by executing `cargo run --example=<day>_<name>`
+directly.
---
+++ src/expense.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+pub fn expense_report(nums: &[i64], target: i64) -> Option<i64> {
+    let mut hash: HashMap<i64, i64> = HashMap::new();
+    for i in nums {
+        if let Some(j) = hash.get(i) {
+            return Some(j * i);
+        } else {
+            hash.insert(target - i, *i);
+        }
+    }
+    None
+}
+
+pub fn triple_expense(nums: &[i64], target: i64) -> Option<i64> {
+    for i in nums {
+        let mut hash: HashMap<i64, i64> = HashMap::new();
+        for j in nums {
+            if j == i {
+                continue;
+            }
+
+            if let Some(k) = hash.get(j) {
+                return Some(i * j * k);
+            } else {
+                hash.insert(target - i - j, *j);
+            }
+        }
+    }
+    None
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn expense_report_satisfies_example() {
+        let res = expense_report(&vec![1721, 979, 366, 299, 675, 1456], 2020);
+        assert_eq!(res, Some(514579));
+    }
+
+    #[test]
+    fn expense_report_out_of_3_values() {
+        let res = triple_expense(&vec![1721, 979, 366, 299, 675, 1456], 2020);
+        assert_eq!(res, Some(241861950));
+    }
+}
